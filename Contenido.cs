b2bb9ac33b5c818acc6adcaac362a1c9154b8fa7
Jaime Stihl <91011915+Jaimome@users.noreply.github.com>
2022-04-17 18:16:51 +0200

    Agregar archivos de proyecto.

---
+++ Contenido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO__Práctica_3
+{
+    internal class Contenido
+    {
+        // ------------------------------> ATRIBUTOS
+        // ------------------------------> CONSTRUCTORES
+        // ------------------------------> MUTADORES
+        // ------------------------------> ACCESORES
+        // ------------------------------> MÉTODOS
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO__Práctica_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ------------------------------> VARIABLES
+            int menu1 = 0;
+            // Contenido
+            string titulo, plataforma, descripcion, url, usuario;
+            int calificacion, genero, likes;
+            // Pelicula
+            string director, distribuidora;
+            int año, duracion;
+            // Serie
+            int temporadas;
+            bool finalizado;
+            // Podcast
+            int fechaCaducidad;
+            bool formato;
+
+            // ------------------------------> CONSTRUCTORES
+            List<Episodio> episodios = new List<Episodio>();
+
+            // ------------------------------> MAIN
+            do
+            {
+                Console.WriteLine("1) Iniciar sesión");
+                Console.WriteLine("2) Salir");
+                Int32.TryParse(Console.ReadLine(), out menu1);
+            } while (menu1 != 2);
+
+            // ------------------------------> MÉTODOS
+
+        }
+    }
+}
